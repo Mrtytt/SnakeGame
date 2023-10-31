5a7925b0c9e3b1c71d4663ccd3ba0cc65b5f75cb
Murat Berk Yetiştirir <muratbrk04@gmail.com>
2023-10-31 14:30:13 +0300

    Add project files.

---
+++ SnakeGame/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace SnakeGame
+{
+    class SnakeGame
+    {
+        private static int ScreenWidth = Console.WindowWidth = 120;
+        private static int ScreenHeight = Console.WindowHeight = 30;
+
+
+        private static Queue<(int X, int Y)> Snake;
+        private static (int X, int Y) Food;
+        private static bool IsGameOver;
+        private static int Score;
+        public static void Main()
+        {
+            Score = 3;
+            Console.CursorVisible = false;
+            Console.SetWindowSize(ScreenWidth, ScreenHeight);
+
+            Snake = new Queue<(int X, int Y)>(Enumerable.Range(0, Score).Select(i => (ScreenWidth / 2 + i, ScreenHeight / 2)));
+            Food = GenerateFoodPosition();
+            IsGameOver = false;
+
+            while (IsGameOver == false)
+            {
+                char ch = Console.ReadKey(true).KeyChar;
+                DrawGame(ch);
+                UpdateSnake(ch);
+                CheckForGameOver();
+                Thread.Sleep(25);
+            }
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine(Score);
+            Console.ReadKey();
+        }
+
+        public static void DrawGame(char ch)
+        {
+            Console.Clear();
+            // Draw the snake
+            foreach (var (x, y) in Snake)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, y);
+                try
+                {
+                    switch (ch)
+                    {
+                        case 'w':
+                            for (int j = 1; j <= Score; j++)
+                            {
+                                if (j == Score)
+                                {
+                                    Console.SetCursorPosition(x, y - (Score + 1));
+                                    Console.WriteLine("0");
+                                }
+                                Console.SetCursorPosition(x, y - j);
+                                Console.WriteLine("*");
+                            }
+                            break;
+                        case 's':
+                            for (int k = 1; k <=Score ; k++)
+                            {
+                                if(k == Score)
+                                {
+                                    Console.SetCursorPosition(x, y);
+                                    Console.WriteLine("0");
+                                }
+                                Console.SetCursorPosition(x, y - k);
+                                Console.WriteLine("*");
+                            }
+                            break;
+                        case 'a':
+                            for (int m = 1; m <= Score; m++)
+                            {
+                                if (m == Score)
+                                {
+                                    Console.SetCursorPosition(x - (Score +1), y);
+                                    Console.WriteLine("0");
+                                }
+                                Console.SetCursorPosition(x-m, y);
+                                Console.WriteLine("*");
+                            }
+                            break;
+                        case 'd':
+                            for (int n = 1; n <= Score; n++)
+                            {
+                                if (n == Score)
+                                {
+                                    Console.SetCursorPosition(x + (Score-1),y);
+                                    Console.WriteLine("0");
+                                }
+                                Console.SetCursorPosition(x+n-2, y);
+                                Console.WriteLine("*");
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                     IsGameOver = true;
+                }
+            }
+
+            // Draw the food
+            Console.SetCursorPosition(Food.X, Food.Y);
+            Console.Write("*");
+        }
+
+        public static void UpdateSnake(char ch)
+        {
+            // Calculate the new head position
+            var head = Snake.Last();
+            int newHeadX, newHeadY;
+            newHeadX = head.X;
+            newHeadY = head.Y;
+            switch (ch)
+            {
+                case 'd':
+                    newHeadX += 1;
+                    break;
+                case 'a':
+                    newHeadX -= 1;
+                    break;
+                case 'w':
+                    newHeadY -= 1;
+                    break;
+                case 's':
+                    newHeadY += 1;
+                    break;
+                case 'q':
+                    IsGameOver = true;
+                    break;
+                default:
+                    break;
+            }
+            Snake.Enqueue((newHeadX % ScreenWidth, newHeadY % ScreenHeight));
+
+            // If the snake has eaten the food, grow the snake
+            if (Snake.Last().Equals(Food))
+            {
+                Score++;
+                Food = GenerateFoodPosition();
+            }
+            else
+            {
+                Snake.Dequeue();
+            }
+        }
+
+        public static void CheckForGameOver()
+        {
+            // Check for collision with the wall
+            var head = Snake.Last();
+            if (head.X < 0 || head.X > ScreenWidth || head.Y < 0 || head.Y > 28)
+            {
+                IsGameOver = true;
+            }
+
+            // Check for collision with the snake's body
+            foreach (var segment in Snake)
+            {
+                if (!segment.Equals(head) && segment.Equals(head))
+                {
+                    IsGameOver = true;
+                }
+            }
+        }
+
+        public static (int X, int Y) GenerateFoodPosition()
+        {
+            var random = new Random();
+            return (random.Next(ScreenWidth), random.Next(ScreenHeight));
+        }
+    } 
+}
---
+++ SnakeGame3/Program.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Drawing;
+using System.Linq;
+
+class Program
+{
+    private static int ScreenWidth = Console.WindowWidth = 120;
+    private static int ScreenHeight = Console.WindowHeight = 30;
+    private static bool IsGameOver;
+    private static int Score;
+    private static (int X, int Y) Food;
+
+    public static void Main()
+    {
+        Score = 1;
+        Console.CursorVisible = false;
+        var snake = new Snake(new Point(Console.WindowWidth / 2, Console.WindowHeight / 2), Score, '0');
+
+        Food = GenerateFoodPosition();
+        Console.SetCursorPosition(Food.X, Food.Y);
+        Console.WriteLine("*");
+
+        while (IsGameOver == false){
+            var ch = Console.ReadKey(true).Key;
+            if (ch == ConsoleKey.Q) break;
+            switch (ch)
+            {
+                case ConsoleKey.W:
+                case ConsoleKey.UpArrow:
+                    snake.Move(Snake.Direction.Up);
+                    break;
+                case ConsoleKey.S:
+                case ConsoleKey.DownArrow:
+                    snake.Move(Snake.Direction.Down);
+                    break;
+                case ConsoleKey.A:
+                case ConsoleKey.LeftArrow:
+                    snake.Move(Snake.Direction.Left);
+                    break;
+                case ConsoleKey.D:
+                case ConsoleKey.RightArrow:
+                    snake.Move(Snake.Direction.Right);
+                    break;
+            }
+            if (snake.Head.Position.X == Food.X && snake.Head.Position.Y == Food.Y)
+            {
+                Food = GenerateFoodPosition();
+                Console.SetCursorPosition(Food.X, Food.Y);
+                Console.WriteLine("*");
+                Score++;
+                snake.Grow();
+            }
+            else
+                snake.Draw();
+            for (int i = 0;i<snake._body.Length;i++)
+                for (int j = i+1+1; j < snake._body.Length; j++)
+                    if (snake._body[i].Position.X == snake._body[j].Position.X && snake._body[i].Position.Y == snake._body[j].Position.Y)
+                        IsGameOver = true;
+        }
+        Console.CursorVisible = true;
+        Console.SetCursorPosition(0, 0);
+        Console.Write("Well Done! Your Score : {0}\nPress enter...",Score);
+        Console.ReadLine();
+
+    }
+    public class SnakePart
+    {
+        public char Symbol { get; set; }
+        public Point Position { get; set; }
+
+        public SnakePart(Point position, char symbol)
+        {
+            Position = position;
+            Symbol = symbol;
+        }
+
+        public void Draw()
+        {
+            try
+            {
+                Console.SetCursorPosition(Position.X, Position.Y);
+                Console.Write(Symbol);
+            }
+            catch (Exception)
+            {
+                IsGameOver = true;
+            }
+        }
+
+        public void Erase()
+        {
+            Console.SetCursorPosition(Position.X, Position.Y);
+            Console.Write(' ');
+        }
+    }
+    public class Snake
+    {
+        public enum Direction { Up, Down, Left, Right }
+
+        public char Symbol { get; set; }
+
+        public SnakePart[] _body;
+        public  SnakePart Head => _body.First();
+        public SnakePart Tail => _body.Last();
+
+        public Snake(Point startingPoint, int length, char symbol = '*')
+        {
+            Symbol = symbol;
+            _body = Enumerable
+                .Range(0, length)
+                .Select(x => new SnakePart(startingPoint, symbol))
+                .ToArray();
+        }
+
+        public void Draw()
+        {
+            foreach (var snakePart in _body)
+            {
+                snakePart.Draw();
+            }
+        }
+        public void Grow()
+        {
+            var newTail = new SnakePart(Tail.Position, Symbol);
+            _body = _body.Append(newTail).ToArray();
+        }
+
+        public void Move(Direction direction)
+        {
+            SnakePart newHead = null;
+            switch (direction)
+            {
+                case Direction.Up:
+                    newHead = new SnakePart(new Point(Head.Position.X,
+                        Head.Position.Y - 1), Symbol);
+                    break;
+                case Direction.Down:
+                    newHead = new SnakePart(new Point(Head.Position.X,
+                        Head.Position.Y + 1), Symbol);
+                    break;
+                case Direction.Left:
+                    newHead = new SnakePart(new Point(Head.Position.X - 1,
+                        Head.Position.Y), Symbol);
+                    break;
+                case Direction.Right:
+                    newHead = new SnakePart(new Point(Head.Position.X + 1,
+                        Head.Position.Y), Symbol);
+                    break;
+            }
+            Tail.Erase();
+            for (var i = _body.Length - 1; i > 0; i--)
+            {
+                _body[i] = _body[i - 1];
+            }
+            _body[0] = newHead;
+        }
+    }
+    public static (int X, int Y) GenerateFoodPosition()
+    {
+        var random = new Random();
+        return (random.Next(ScreenWidth), random.Next(ScreenHeight));
+    }
+}
